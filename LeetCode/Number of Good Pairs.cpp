129e30ae38810570c501d143f6d94f869200c753
Ellie <ytaghavi@yahoo.com>
2021-04-10 14:39:29 -0700

    Minor Chnages

--- LeetCode/Number of Good Pairs.cpp
+++ LeetCode/Number of Good Pairs.cpp
@@ -1,6 +1,7 @@
+#include <vector>
 class Solution {
 public:
-    int numIdenticalPairs(vector<int>& nums) 
+    int numIdenticalPairs(std::vector<int>& nums) 
     {
         int out = 0;
         for(int i = 0; i < nums.size()-1; ++i)
