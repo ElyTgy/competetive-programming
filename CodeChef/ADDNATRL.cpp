b4e0a0b3110768d0d4d7dc24f960d5710e56c8d9
Ellie <ytaghavi@yahoo.com>
2021-10-13 23:47:06 -0700

    ADDNATRL

---
+++ CodeChef/ADDNATRL.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ar array
+#define ll long long
+#define ld long double
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+
+
+void solve() {
+    long long n;
+    std::cin>>n;
+    if(n % 2 == 0)
+    {
+        std::cout<<(n/2)*(1+n);
+    }
+    else
+    {
+        std::cout<<((n/2)*(1+n)) + (n/2 + 1);
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int tc = 1;
+    //cin >> tc;
+    for (int t = 1; t <= tc; t++) {
+        // cout << "Case #" << t << ": ";
+        solve();
+    }
+}
