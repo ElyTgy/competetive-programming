d63604ffc190324c639b31b8c76b2eab9764f57e
Ellie <ytaghavi@yahoo.com>
2022-03-22 00:45:21 -0700

    350B

--- CodeForces/350B.cpp
+++ CodeForces/350B.cpp
@@ -4,8 +4,12 @@
 using namespace std;
 typedef unsigned long long ull;
 
+const long long N = 100000;
+
 vector<int> hotels;
 vector<int> maxPath = {};
+vector<int> graph[N];
+vector<int> graphNew[N];
 
 void solve();
 int main()
@@ -24,7 +28,6 @@ int main()
 
 void solve()
 {
-    unordered_map<int, int> graph;
     int n;
     cin >> n;
     for (int i = 1; i <= n; ++i)
@@ -35,30 +38,38 @@ void solve()
     }
     for (int i = 1; i <= n; ++i)
     {
-        int v = 0;
+        int v;
         cin >> v;
         if (v != 0)
         {
-            graph[i] = v;
+            graph[v].push_back(i);
         }
     }
+
+    for (int i = 1; i < n; ++i)
+    {
+        for(int j = 0; j < graph[i].size(); ++j)
+        {
+            graphNew[graph[i][j]] = {i};
+        }
+    }
+
     for (int i = 0; i < hotels.size(); ++i)
     {
         vector<int> currPath;
         int currV = hotels[i];
-        while (graph.find(currV) != graph.end())
+        while (graphNew[currV].size() != 0 && graph[graphNew[currV][0]].size() == 1)
         {
             currPath.push_back(currV);
-            currV = graph[currV];
+            currV = graphNew[currV][0];
         }
         currPath.push_back(currV);
         if (currPath.size() > maxPath.size()) { maxPath = currPath; }
     }
 
-    std::cout << maxPath.size()<<'\n';
-    for (int i = maxPath.size()-1; i >= 0; --i)
+    std::cout << maxPath.size() << '\n';
+    for (int i = maxPath.size() - 1; i >= 0; --i)
     {
-        std::cout << maxPath[i] << " " <<;
+        std::cout << maxPath[i] << " ";
     }
-
 }
